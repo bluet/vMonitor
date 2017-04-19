18e79eb20635801489bd5f901e2b3cfa36e7976f
BlueT - Matthew Lien - 練喆明 <BlueT@BlueT.org>
2017-04-19 19:05:52 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+"use strict";
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
+var vsphere = require("./vendor/vsphere-sdk/dist/vsphere.js");
+
+var hostname = "",
+    username = "",
+    password = "";
+
+vsphere.vimService(hostname).then(function(service) {
+	var propertyCollector = service.serviceContent.propertyCollector,
+	    rootFolder = service.serviceContent.rootFolder,
+	    sessionManager = service.serviceContent.sessionManager,
+	    viewManager = service.serviceContent.viewManager,
+	    vim = service.vim,
+	    vimPort = service.vimPort;
+	return vimPort.login(sessionManager, username, password)
+	.then(function() {
+		return vimPort.createContainerView(viewManager, rootFolder,
+		[ "VirtualMachine" ], true);
+	})
+	.then(function(containerView) {
+		return vimPort.retrievePropertiesEx(propertyCollector, [
+			vim.PropertyFilterSpec({
+				objectSet: vim.ObjectSpec({
+					obj: containerView,
+					skip: true,
+					selectSet: vim.TraversalSpec({
+						path: "view",
+						type: "ContainerView"
+					})
+				}),
+				propSet: vim.PropertySpec({
+					type: "VirtualMachine",
+					pathSet: [ "name", "guest", "guestHeartbeatStatus", "resourceConfig", "storage", "summary", "overallStatus", "triggeredAlarmState" ]
+				})
+			})
+		], vim.RetrieveOptions());
+	})
+	.then(function(result) {
+		result.objects.forEach(function(item) {
+			//~ console.log(item.propSet[0].val);
+			//~ console.log("%o", item.propSet);
+			console.log(JSON.stringify(item.propSet, null, 4));
+		});
+		return vimPort.logout(sessionManager);
+	});
+}).catch(function(err) {
+	console.log(err.message);
+});
+
+
+// http://pubs.vmware.com/vsphere-60/index.jsp?topic=/com.vmware.wssdk.apiref.doc/index.html&single=true
+// https://labs.vmware.com/flings/vsphere-sdk-for-javascript?download_url=https%3A%2F%2Fdownload3.vmware.com%2Fsoftware%2Fvmw-tools%2Fvsphere-sdk-for-javascript%2Fvsphere-1.1.0-src.tgz#comments
+// https://download3.vmware.com/software/vmw-tools/vsphere-sdk-for-javascript/vsphere-1.1.0-src.tgz
